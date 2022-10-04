9410fa517e9845ee8d18a609676b608c2b089997
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2022-10-04 23:43:25 +0500

    Add files via upload

---
+++ soloLearn_while_loop_game_control.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+"""here is the game mode.
+you are making a game, in which player tries to shoot something, so either he misses or hit it.
+The player starts with 100 points. 
+Each hit will add 10 points to the total while any miss will result in deduciton of points.
+
+Generate program and take 4 inputs of either Hit or miss, and output the required."""
+
+# here code starts
+#start the code with no of initiator as zero.
+
+i=0
+points=100
+
+#starting while loop, for total no of shoots=4;
+#initiator i<4 means, starting from 0, 1, 2, 3
+
+while i <4:
+    shoot=input("Enter Hit, if it's done \n") #\n is for new lines
+    if shoot=="Hit":
+        points+=10
+        i+=1
+    else:
+        points-=20
+        i+=1
+print("The Total Points will be",points)
+
+
+# In[ ]:
+
+
+
+
