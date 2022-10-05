7a765b35ebdf409e10e820f3674ff3b7a99bba4e
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2022-10-05 21:47:32 +0500

    Add files via upload

---
+++ while_loop and no of inputs restricted (calculator).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+'''Here is the problem statement.
+
+You are making a ticketing system.
+The price of single ticket is $100.
+For children under age 3 years old, the ticket is free.
+
+Your program needs to take the ages of 5 passengers as input
+and output the total price of the tickets'''
+
+total=0
+i=5
+while i>0:
+    i=i-1
+    age=int(input("Enter age \n"))
+    if age>3:
+        total+=100
+print("The total price will be $",total)
+
+
+# 
