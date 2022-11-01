cd97e3ad5d6658a721ac03e76805b5afa4675fc5
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2022-11-01 23:06:56 +0500

    Create string_slicing.py

---
+++ string_slicing.py
@@ -0,0 +1,9 @@
+string="Good man."
+       #012345678   indexing reference (string length is 9)
+
+print(string[:])   #complete stirng will be printed
+print(string[::])  #complete string will be printed
+
+print(string[:4])  #upto index 4 (exludeding index 4)
+
+print(string[5:8]) #from index 5 (included) upto index 8(excluded)
